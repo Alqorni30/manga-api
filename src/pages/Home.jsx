d1218e15c6edf9b424332cb9e3a2a8d684eb7277
Hervinsa Alqorni <vinnbayo@gmail.com>
2023-10-25 23:22:45 +0700

    hapus impor rekomendasi

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Topmanga from "../components/Topmanga";
 import Header from "../components/Header";
-import Rekomendasi from "../components/Rekomendasi";
 
 const Home = () => {
   return (
@@ -9,7 +8,6 @@ const Home = () => {
       <section className="px-4">
         <Header linkHref="/populer" linkTitle="Lihat Semua" title="Populer" />
         <Topmanga />
-        <Rekomendasi/>
       </section>
     </>
   );
